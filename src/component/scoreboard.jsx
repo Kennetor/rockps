0101917d9fe2648532f2a58da048f5b8764fb538
kenneth <kenneth1.kodehode@gmail.com>
2023-01-23 11:19:11 +0100

    e

--- src/component/scoreboard.jsx
+++ src/component/scoreboard.jsx
@@ -1,6 +1,7 @@
 // Contexts
 import { useContext } from "react";
 import ScoreContext from "../contexts/ScoreContext";
+import Logo from "../component/img/logo.svg";
 
 function Scoreboard() {
   const { score } = useContext(ScoreContext);
@@ -9,7 +10,7 @@ function Scoreboard() {
     <>
       <div className="border-2 rounded-xl p-4 flex border-blue-300 m-auto -mt-96 xl:-mt-[30rem] xl:scale-110 xl:right-60 right-2">
         <div className="xl:text-5xl text-3xl p-2 text-left xl:w-[33rem] ">
-          <img src="/images/logo.svg" className=" xl:h-40 scale"></img>
+          <img src={Logo} className=" xl:h-40 scale"></img>
         </div>
         <div className="rounded-xl border-2 xl:ml-44 w-1/3 bg-white text-3xl grid text-slate-800 ml-24">
           <p className="text-2xl xl:mt-4 mt-2 xl:w w-20 m-auto">Score</p>
